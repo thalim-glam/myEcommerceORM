f900d0c3bbb467c1cbbec20f8eeedb261984a2ee
Tasneem <thalim.glam@gmail.com>
2023-12-19 09:29:50 -0800

    Working on tag-route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,11 +47,30 @@ router.post('/', async(req, res) => {
 
 router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
-  
+  try{
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id:req.params.id,
+      },
+    });
+    res.status(200).json(tagData);
+  }catch(error){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(tagData);
+  }catch(error){
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
